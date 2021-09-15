844bc29339706fd70c81f1082175b33280b41186
Maria78900 <90794984+Maria78900@users.noreply.github.com>
2021-09-15 16:32:31 -0300

    Add files via upload

---
+++ logica.py
@@ -0,0 +1,18 @@
+user = "Mariana"
+Number = 50
+saldo= 2000
+limite=5000
+print(user,Number,saldo,limite)
+
+
+user = "Pedro"
+Number = 50
+saldo= 900
+limite=5000
+print(user,Number,saldo,limite)
+
+user = "Mario"
+Number = 55
+saldo= 600
+limite=5000
+print(user,Number,saldo,limite)
